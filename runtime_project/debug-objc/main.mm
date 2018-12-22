85328dbcb079fd922a25c72d5d1803808bedd354
fangshufeng <fangsh@dianrong.com>
2018-12-22 09:05:18 +0800

    rm test code

--- runtime_project/debug-objc/main.mm
+++ runtime_project/debug-objc/main.mm
@@ -6,25 +6,10 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "fake_objc.h"
-#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        
-        Person *p =[[Person alloc] init];
-        
-        unsigned long a = (unsigned long)@selector(test1);
-        NSLog(@"---%ld",a);
-        [p test1];
-        
-        objc_class;
-        
-        fake_objc_class *tmp = (__bridge fake_objc_class *)[p class];
-        
-        [p test1];
-        
+
     }
     return 0;
 }
