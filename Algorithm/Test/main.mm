620f76fe3b42b91b3a280a8e34784c8d4a9dd510
fangshufeng <fangsh@dianrong.com>
2019-05-28 17:36:41 +0800

    选择排序

---
+++ Algorithm/Test/main.mm
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test
+//
+//  Created by fangshufeng on 2019/5/7.
+//  Copyright © 2019 fangshufeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- BaseOcSyntaxDemo/BaseOcSyntaxDemo/ViewController.m
+++ BaseOcSyntaxDemo/BaseOcSyntaxDemo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "Son.h"
+#import <WebKit/WebKit.h>
 
 @interface ViewController ()
 
@@ -24,11 +25,20 @@
     
     Son *son  = [[Son alloc] init];
     
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
     
-    
-        
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+    NSURL *url = [NSURL URLWithString:@"http://huojianshiwan.com/#/withdrawMoney"];
+    [webView loadRequest:[NSURLRequest requestWithURL:url]];
+    [self.view addSubview:webView];
 }
 
+- (void)keyboardWillHide {
+    NSLog(@"keyboardWillHide");
+}
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
 //    abort();
